8ce140e6c072a63641ee747038d4da06c476cdb0
Lammia00 <70175691+Lammia00@users.noreply.github.com>
2021-03-28 23:30:35 +0300

    28/3/2021

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -27,6 +27,17 @@ export class AccountService {
     )
   }
 
+  register(model: any){
+    //return this.http.post<User>(this.baseUrl + 'account/register',model) // added <user> so the type can be clear
+    return this.http.post<User>(this.baseUrl + 'account/register',model).pipe(
+      map((user:User)=> {
+        if (user) {
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      }) 
+    )
+  }
   setCurrentUser(user: User){
     this.currentUserSource.next(user);
   }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  users: any;
+ 
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
@@ -19,7 +19,12 @@ export class HomeComponent implements OnInit {
     this.registerMode =!this.registerMode;
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
+ // getUsers(){
+ //   this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
+ // }
+
+  cancelRegisterMode(event: boolean){
+    this.registerMode = event;
   }
+
 }
